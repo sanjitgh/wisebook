fd383986f210045b31fa0617056cc584d75e7e2a
Sanjit Ghosh <sanjitkumarghosh0@gmail.com>
2024-12-27 22:05:21 +0600

    integrate cupon code field in single product page

--- js/singleProduct.js
+++ js/singleProduct.js
@@ -1,8 +1,8 @@
 document.querySelectorAll(".thumbnail").forEach((thumbnail) => {
-    thumbnail.addEventListener("click", function () {
-        const mainImage = document.querySelector(".product-main-image");
-        mainImage.src = this.src;
-    });
+  thumbnail.addEventListener("click", function () {
+    const mainImage = document.querySelector(".product-main-image");
+    mainImage.src = this.src;
+  });
 });
 
 ///
@@ -14,52 +14,64 @@ const secondModal = document.getElementById('second_modal_myModal');
 
 // Open modal
 secondModalOpenBtn.addEventListener('click', () => {
-    secondModal.style.display = 'block';
+  secondModal.style.display = 'block';
 });
 
 // Close modal
 secondModalCloseBtn.addEventListener('click', () => {
-    secondModal.style.display = 'none';
+  secondModal.style.display = 'none';
 });
 
 // Close modal when clicking outside of the modal content
 window.addEventListener('click', (event) => {
-    if (event.target === secondModal) {
-        secondModal.style.display = 'none';
-    }
+  if (event.target === secondModal) {
+    secondModal.style.display = 'none';
+  }
 });
 
 ///////////////////////////////////
 function copyToClipboard() {
-    // Get the textarea element
-    var copyText = document.getElementById("embedCode");
+  // Get the textarea element
+  var copyText = document.getElementById("embedCode");
 
-    // Use the Clipboard API to copy the content
-    navigator.clipboard.writeText(copyText.value).then(function() {
-      // Success callback
-      var alertBox = document.getElementById("customAlert");
-      alertBox.classList.add("show-alert");
+  // Use the Clipboard API to copy the content
+  navigator.clipboard.writeText(copyText.value).then(function () {
+    // Success callback
+    var alertBox = document.getElementById("customAlert");
+    alertBox.classList.add("show-alert");
 
-      // Hide the alert after 2 seconds
-      setTimeout(function() {
-        alertBox.classList.remove("show-alert");
-      }, 2000);
-    }).catch(function(err) {
-      // Error callback
-      alert("Failed to copy text: " + err);
-    });
-  }
+    // Hide the alert after 2 seconds
+    setTimeout(function () {
+      alertBox.classList.remove("show-alert");
+    }, 2000);
+  }).catch(function (err) {
+    // Error callback
+    alert("Failed to copy text: " + err);
+  });
+}
 
 //   ////////////////////////////
 
 
 const selectElement = document.getElementById('deliveryOption');
-    const localLink = document.getElementById('localLink');
+const localLink = document.getElementById('localLink');
 
-    selectElement.addEventListener('change', () => {
-      if (selectElement.value === 'pickup') {
-        localLink.classList.remove('d-none'); // Show the link
-      } else {
-        localLink.classList.add('d-none'); // Hide the link
-      }
-    })
+selectElement.addEventListener('change', () => {
+  if (selectElement.value === 'pickup') {
+    localLink.classList.remove('d-none'); // Show the link
+  } else {
+    localLink.classList.add('d-none'); // Hide the link
+  }
+})
+
+// input refer js
+document.addEventListener("DOMContentLoaded", function () {
+  const referBtn = document.getElementById("referBtn");
+  const referenceInput = document.getElementById("referenceInput");
+
+  referBtn.addEventListener("click", function (event) {
+    event.preventDefault();
+    referBtn.style.display = "none";
+    referenceInput.style.display = "block";
+  });
+});
